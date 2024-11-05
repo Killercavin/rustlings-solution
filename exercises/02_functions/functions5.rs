622a4bd5c1f0f9a3732a2aebb59b9920dd36ec3a
Cavin <cavinlarry001@gmail.com>
2024-11-05 12:08:57 +0300

    Add initial solutions and exercises for Rust programming concepts

---
+++ exercises/02_functions/functions5.rs
@@ -0,0 +1,11 @@
+// TODO: Fix the function body without changing the signature.
+fn square(num: i32) -> i32{
+    num * num
+}
+
+fn main() {
+    let answer = square(3);
+    println!("The square of 3 is {answer}");
+}
+
+// this excerise is about function signature, the function signature is the function name, the parameter, and the return type
